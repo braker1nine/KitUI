39b3f121ecaac365083ea55780fc1aa1b739fffc
Chris Brakebill <git@bkbl.me>
2023-05-18 14:49:46 -0400

    Add the ability to disable visual effects on UIVisualEffectViews

--- Sources/KitUI/Extensions/UIKit/UIVisualEffectView.swift
+++ Sources/KitUI/Extensions/UIKit/UIVisualEffectView.swift
@@ -1,11 +1,21 @@
 import Foundation
+import ReactiveSwift
 import UIKit
 
 public func VisualEffect(
     effect: UIVisualEffect = UIBlurEffect(style: .regular),
+    disable: some SignalProducerConvertible<Bool, Never> = false,
     content: () -> UIView
 ) -> UIView {
     let wrap = UIVisualEffectView(effect: effect)
+    disable.producer.take(duringLifetimeOf: wrap).startWithValues { [weak wrap] disabled in
+        guard let wrap = wrap else { return }
+        if disabled {
+            wrap.effect = nil
+        } else {
+            wrap.effect = effect
+        }
+    }
     let contentView = content()
     wrap.contentView.addSubview(contentView)
     contentView.edgesToSuperview()
@@ -15,11 +25,15 @@ public func VisualEffect(
 public func Vibrancy(
     blurEffect: UIBlurEffect,
     style: UIVibrancyEffectStyle,
+    disable: some SignalProducerConvertible<Bool, Never> = false,
     content: () -> UIView
 ) -> UIView {
     
     let effect = UIVibrancyEffect(blurEffect: blurEffect, style: style)
     let wrap = UIVisualEffectView(effect: effect)
+    disable.producer.take(duringLifetimeOf: wrap).startWithValues { [weak wrap] disabled in
+        wrap?.effect = disabled ? nil : effect
+    }
     let view = content()
     wrap.contentView.addSubview(view)
     view.edgesToSuperview()
@@ -27,13 +41,17 @@ public func Vibrancy(
 }
 
 extension UIView {
-    public func vibrancy(blurEffect: UIBlurEffect.Style, style: UIVibrancyEffectStyle) -> UIView {
+    public func vibrancy(
+        blurEffect: UIBlurEffect.Style,
+        style: UIVibrancyEffectStyle,
+        disable: some SignalProducerConvertible<Bool, Never> = false
+    ) -> UIView {
         Vibrancy(blurEffect: .init(style: blurEffect), style: style) {
             self
         }
     }
     
-    public func visualEffect(_ effect: UIVisualEffect) -> UIView {
-        VisualEffect(effect: effect, content: { self })
+    public func visualEffect(_ effect: UIVisualEffect, disable: some SignalProducerConvertible<Bool, Never> = false) -> UIView {
+        VisualEffect(effect: effect, disable: disable, content: { self })
     }
 }
